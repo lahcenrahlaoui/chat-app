4a8ca07c220758ee50c2fca632b6662dc05c3f0e
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2024-01-13 21:47:32 +0100

    add time to schema

--- models/messageModel.js
+++ models/messageModel.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const messageModel = new Schema({
-    content: String,
-    from: String,
-    to: String
-});
+const messageModel = new Schema(
+    {
+        content: String, 
+        from: String,
+        to: String,
+    },
+    { timestamps: true }
+);
 
 const Message = mongoose.model("Message", messageModel);
 module.exports = Message;
