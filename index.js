f817f85a6b10677fe8936700e7a7560aae247a7e
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2024-01-16 14:42:22 +0100

    add the socket io

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+/// start  this
+const http = require("http");
+const { Server } = require("socket.io");
+// end this
+
 //files imports
 const messagesRoute = require("./routes/messagesRoute.js");
 const userRoute = require("./routes/userRoute.js");
 
 // create the app
 const app = express();
+
+// start this
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://social-media-client-blue.vercel.app", // string[]
+        methods: ["GET", "POST"],
+        allowedHeaders: [],
+        credentials: true,
+    },
+});
+// end this
 
 //middlewares
 app.use(express.static("public"));
@@ -39,8 +56,14 @@ app.use("/api/users", userRoute);
 
 mongoose
     .connect(MONGO_URL)
-    .then(() =>
-        app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+    .then(
+        () => {
+            // start this
+            server.listen(PORT);
+            io.on("connection", () => {});
+            // end this
+        }
+        // app.listen(PORT, () => console.log(`listening on port ${PORT}`))
     )
     .catch((err) => {
         console.log("database connection error", err);
